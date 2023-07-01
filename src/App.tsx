613dbb136e977943ed629818ab954dfe2c58ea7c
Cristina Rojas <cristina.rojas@datastax.com>
2023-07-01 11:06:35 -0700

    Adding photo and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import mainPhoto from './photos/mainMom.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Welcome to - The AI Mom -</h4>
       </header>
+      <body>
+        <section className="App-body">
+          <section className="App-main-photo"> 
+              <img src={mainPhoto} className="App-logo" alt="logo" />
+          </section>
+          <section className="App-main-chat"> 
+              <h1>Ask me anything!</h1>
+          </section>
+        </section>
+      </body>
     </div>
   );
 }
